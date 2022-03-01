eafd93d3d2b1d38479f7e6c33dbd9961d67641ee
JishnuKher <jishkher@yahoo.co.in>
2022-03-01 18:47:45 -0500

    Fixing a bug, and adding mainmenu script
    Just had a minor bug with a missing object, also connected script to buttons

---
+++ Sinoda/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Starting Game");
+        SceneManager.LoadScene("Gamescene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        SceneManager.LoadScene("Quit");
+    }
+}
