9b2fa9b1a91d3f36a4c9b8ff2de0addba3d9ad88
JishnuKher <jishkher@yahoo.co.in>
2022-03-19 16:25:31 -0400

    Making a fix in loadgame.cs file in order to fix compilation error
    Needed to fix an error in the loadgame.cs file in develop in order to get scripts to compile and to fix future merge conflicts

--- Sinoda/Assets/Scripts/LoadGame.cs
+++ Sinoda/Assets/Scripts/LoadGame.cs
@@ -6,14 +6,17 @@ using System.IO;
 public class LoadGame : MonoBehaviour
 {
     private GameObject[] obj;
-    private string static path = "Assets/savefile.txt";
+    private static string path = "Assets/savefile.txt";
     private StreamReader reader = new StreamReader(path);
     // Update is called once per frame
     void Update()
     {
         foreach (GameObject x in obj)
         {
-            Instantiate(piecePrefab, x.transform.position, x.transform.rotation);
+            //commented out this line for now as prefab is not being detected due to editor bug
+          //  Instantiate(piecePrefab, x.transform.position, x.transform.rotation);
+
+          Debug.Log("Loading Game Object");
         }
     }
 }
