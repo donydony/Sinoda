54d7ca9bc562601a8a822f92829817b4b4ed8866
shadowshadow725 <shadowshadow725@gmail.com>
2022-03-26 17:04:44 -0400

    reset to develop

--- Sinoda/Assets/Scripts/Board.cs
+++ Sinoda/Assets/Scripts/Board.cs
@@ -3,196 +3,298 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(HitBoxGenerater))]
 public class Board: MonoBehaviour
 {
+    [SerializeField] static private double[] ca = new double[] { -42.6, -24.7, -6.6, 12.6, 29.9, 49.7 };
+    [SerializeField] static private double[] cb = new double[] { -49.6, -31.9, -12.7, 5.7, 24.1, 43.6 };
+    [SerializeField] static private double[] r = new double[] { 42.1, 33.6, 24.7, 16.8, 8.3, 0, -8.3, -16.8, -24.7, -33.6, -42.1 };
+    [SerializeField] static private double y = 3;
+    [SerializeField] const int num_slots = 54;
+    static private double[] xs = new double[] {
+        ca[0], cb[0], ca[0], cb[0], ca[0], cb[0], ca[0],
+        ca[1], cb[1], ca[1], cb[1], ca[1], cb[1], ca[1],cb[1],ca[1],
+        ca[2], cb[2], ca[2], cb[2], ca[2], cb[2], ca[2],cb[2],ca[2],cb[2],ca[2],
+        cb[3], ca[3], cb[3], ca[3], cb[3], ca[3],cb[3],ca[3],cb[3],ca[3],cb[3],
+        cb[4], ca[4], cb[4], ca[4], cb[4], ca[4],cb[4],ca[4],cb[4],
+        cb[5], ca[5], cb[5], ca[5], cb[5], ca[5],cb[5],
+    };
+    static private int[] ifturn = new int[] {
+        0,1,0,1,0,1,0,
+        0,1,0,1,0,1,0,1,0,
+        0,1,0,1,0,1,0,1,0,1,0,
+        1,0,1,0,1,0,1,0,1,0,1,
+        1,0,1,0,1,0,1,0,1,
+        1,0,1,0,1,0,1
+    };
 
-    [SerializeField] public int player_count;
-    private Slots[] slots;
-    private Player[] _players;
-    private Piece[] pieces;
-    [SerializeField] public int turn;
-    private int winner;
-    private int playerCount = 2;
-
-    // Start is called before the first frame update
-    void Start()
+    static private double[] ys = new double[]{
+                      r[8], r[7], r[6], r[5], r[4], r[3], r[2],
+                r[9], r[8], r[7], r[6], r[5], r[4], r[3], r[2], r[1],
+         r[10], r[9], r[8], r[7], r[6], r[5], r[4], r[3], r[2], r[1], r[0],
+         r[10], r[9], r[8], r[7], r[6], r[5], r[4], r[3], r[2], r[1], r[0],
+                r[9], r[8], r[7], r[6], r[5], r[4], r[3], r[2], r[1],
+                      r[8], r[7], r[6], r[5], r[4], r[3], r[2],
+     };
+    static private int[,] adjacency = new int[,]{   { -1, 2, 9 }, // 1
+                { 3, 1, -1 }, 
+                { 2, 4, 11 }, 
+                { 3, 5, -1 }, 
+                { 4, 6, 13 },
+                { 7, 5, -1 },
+                { 6, 15, -1 },
+                { 18, 9, -1 }, // 8
+                { 8, 10, 1 },
+                { 9, 20, 11 },
+                { 3, 10, 12 },
+                { 13, 11, 22 },
+                { 14, 5, 12 },
+                { 13, 15, 24 },
+                { 14, 16, 7 },
+                { 26, 15, -1 },
+                { 18, 28, -1 }, // 17
+                { 19, 8, 17 },
+                { 18, 20, 30 },
+                { 21, 19, 10 },
+                { 32, 20, 22 },
+                { 23, 21, 12 },
+                { 34, 22, 24 },
+                { 23, 25, 14 },
+                { 24, 26, 36 },
+                { 27, 25, 16 },
+                { 26, 38, -1 },
+                { 17, 29, -1 }, // 28
+                { 28, 30, 39 },
+                { 31, 29, 19 },
+                { 30, 32, 41 },
+                { 31, 33, 21 },
+                { 32, 34, 43 },
+                { 33, 35, 23 },
+                { 34, 36, 45 },
+                { 37, 35, 25 },
+                { 36, 38, 47 },
+                { 37, 27, -1 },
+                { 29, 40, -1 }, // 39
+                { 39, 41, 48 },
+                { 40, 31, 42 },
+                { 41, 43, 50 },
+                { 33, 42, 44 },
+                { 43, 45, 52 },
+                { 35, 44, 46 },
+                { 45, 47, 54 },
+                { 37, 46, -1 },
+                { 40, 49, -1 }, // 48
+                { 48, 50, -1 },
+                { 49, 51, -1 },
+                { 52, 50, -1 },
+                { 53, 51, -1 },
+                { 52, 54, -1 },
+                { 46, 53, -1 }
+            };
+//==================================================================================================
+private Piece[] Grid;
+    private Piece SelectedPiece;
+    private GameController controller;
+    private HitBoxGenerater hitboxcreator;
+    private bool winstats;
+    private void Awake()
     {
-        
+        CreateBoard();
+        winstats = false;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SetController(GameController c)
     {
-        
-        
+        hitboxcreator = GetComponent<HitBoxGenerater>();
+        this.controller = c;
     }
-
-    public void Board_start()
+    private void CreateBoard()
     {
-        Debug.Log("Initialize board");
-        this.turn = 0;
-        this.player_count = playerCount;
-        this.winner = -1;
-        this._players = new Player[playerCount];
-        if (playerCount > 3)
+        Grid = new Piece[num_slots];
+    }
+    public void AddPieceToBoard(int slot, Piece p)
+    {
+       Grid[slot-1] = p;
+    }
+    public void HandlePiece(Piece p)
+    {
+        if (!winstats)
         {
-            this.slots = new Slots[54];
-            for (int i = 0; i < 54; i++)
+            if (SelectedPiece)
             {
-                // this.slots[i] = new Slots(i);
-            }
+                if (SelectedPiece.slot == p.slot)
+                {
+                    DeselectPiece();
+                }
+                else
+                {
+                    if (controller.IfTurnTrue(p.owner))
+                    {
+                        DeselectPiece();
+                        SelectPiece(p);
+                    }
 
-            BigBoardInit bi = new BigBoardInit();
-            bi.loadAdjcency(this.slots);
-        }
-        else
-        {
-            this.slots = new Slots[24];
-            for (int i = 0; i < 24; i++)
+                }
+
+            }
+            else
             {
-                this.slots[i] = new Slots(i);
+                if (controller.IfTurnTrue(p.owner))
+                {
+                    SelectPiece(p);
+                }
             }
+        }
+    }
+    public void HandleHitBox(HitBox h)
+    {
+        if (SelectedPiece)
+        {
+            Moving(h.slot);
+        }
+    }
 
-            SmallBoardInit si = new SmallBoardInit();
-            si.loadAdjcency(this.slots);
+    public void SelectPiece(Piece p)
+    {
+        SelectedPiece = p;
+        p.Selected(true);
+        GenerateHitBox();
+    }
 
-            // load initial board for 2 players 
+    public void DeselectPiece()
+    {
+        SelectedPiece.Selected(false);
+        SelectedPiece = null;
+        hitboxcreator.close();
+    }
+    public void GenerateHitBox()
+    {
+        hitboxcreator.close();
+        foreach (var i in SelectedPiece.availablemoves)
+        {
+            HitBox b = hitboxcreator.show().GetComponent<HitBox>();
+            b.SetData(GetPositionAtSlot(i), i, IfTurnAtSlot(i), this);
+        }
+    }
+    public void SwitchTurn()
+    {
+        controller.SwitchTurn();
+    }
 
-            TwoPlayerConfiguration p2 = new TwoPlayerConfiguration();
-            this.pieces = new Piece[12];
-            for (int i = 0; i < 12; i++)
+    public Vector3 GetPositionAtSlot(int slot)
+    {
+        
+        return new Vector3((float)xs[slot - 1], (float)y, (float)ys[slot - 1]);
+    }
+
+    public int IfTurnAtSlot(int slot)
+    {
+        return ifturn[slot - 1];
+    }
+    public Piece GetPieceBySlot(int slot)
+    {
+        return Grid[slot-1];
+    }
+
+    public void Moving(int slot)
+    {
+        if (hasenemypiece(slot,SelectedPiece.owner))
+        {
+            CapturedPiece(slot);
+        }
+        Grid[slot-1] = SelectedPiece;
+        Grid[SelectedPiece.slot-1] = null;
+        SelectedPiece.moveto(slot);
+        SelectedPiece = null;
+        hitboxcreator.close();
+        SwitchTurn();
+    }
+
+    public bool hasenemypiece(int slot, int o)
+    {
+        if (haspiece(slot))
+        {
+            if(Grid[slot-1].owner != o)
             {
-                this.pieces[i] = new Piece(i, i % 6);
+                return true;
             }
-            p2.load(this.slots, this.pieces);
-            Debug.Log("Load complete");
         }
-
-        for (int i = 0; i < this.player_count; i++)
+        return false;
+            
+    }
+    public bool hasfriendpiece(int slot, int o)
+    {
+        if (haspiece(slot))
         {
-            this._players[i] = new Player(i);
+            if (Grid[slot - 1].owner == o)
+            {
+                return true;
+            }
         }
-        Debug.Log("Initialize Complete");
-
-
-
+        return false;
     }
-    //public int returnSlotsnum()
-    //{
-    //}
-    public void passTurn()
+    public bool haspiece(int slot)
     {
-        turn = (turn + 1) % this.player_count;
-    }
-
-    
-    public bool getSlotOccupancy(int id)
-    {
-        return this.slots[id].havePiece;
-        
-    }
-
-    public int getSlotPieceOwner(int id)
-    {
-        if (!this.slots[id].havePiece)
-        {
-            return -1;
-        }
-        return this.slots[id].piece.owner;
-    }
-    public Slots[] returnSlots()
-    {
-        return this.slots;
-    }
-
-    public int getCurrentPlayer()
-    {
-        return this.turn;
-    }
-
-    public int getWinner()
-    {
-        return this.winner;
-    }
-
-    public bool checkValidMove(int current_slot_id, int destination_slot)
-    {
-        
-        // Slots current_Slot = this.slots[current_slot_id];
-        // Piece current_Piece = current_Slot.piece;
-        // int current_Value = current_Piece.value;
-        // if (current_Value == 1){
-        //     return current_Slot.adjcents.Contains(destination_slot);
-        // } else if (current_Value == 2){
-        //     if (current_slot_id == destination_slot){
-        //         return false;
-        //     }
-        //     foreach (int value in current_Slot.adjcents){
-        //         if (this.slots[value].adjcents.Contains(destination_slot)){
-        //             return true;
-        //         }
-        //     }
-        //     return false;
-        // } else if (current_Value == 3){
-        //     if (current_Slot.adjcents.Contains(destination_slot)){
-        //         return false;
-        //     }
-        //     foreach (int value in current_Slot.adjcents){
-        //         foreach (int value1 in this.slots[value].adjcents){
-        //             if (this.slots[value1].adjcents.Contains(destination_slot)){
-        //                 return true;
-        //             }
-        //         }
-        //     }
-        //     return false;
-        // } else if (current_Value == 4){
-        //     List<int> first_Layer = current_Slot.adjcents;
-        //     List<int> second_Layer;
-        //     List<int> third_Layer;
-        //     List<int> fourth_Layer;
-        //     foreach (int value in first_Layer){
-        //         second_Layer = second_Layer.Concat(this.slots[value].adjcents).ToList();
-        //     }
-        //     second_Layer.Remove(current_slot_id);
-        //     foreach (int value in second_Layer){
-        //         third_Layer = third_Layer.Concat(this.slots[value].adjcents).ToList();
-        //     }
-        //     third_Layer = third_Layer.Except(second_Layer);
-        //     foreach (int value in third_Layer){
-        //         fourth_Layer = fourth_Layer.Concat(this.slots[value].adjcents).ToList();
-        //     }
-        //     fourth_Layer = fourth_Layer.Except(third_Layer);  
-        // }
-
-        return true;
-        
-    }
-
-    public bool move(int current_slot_id, int destination_slot)
-    {
-        // check and move valid and move the piece 
-        if (!checkValidMove(current_slot_id, destination_slot))
+        if (Grid[slot - 1] == null)
         {
             return false;
         }
-        this.slots[destination_slot].piece = this.slots[current_slot_id].piece;
-        this.slots[destination_slot].havePiece = true;
-        this.slots[current_slot_id].piece.slot = destination_slot;
-        this.slots[current_slot_id].havePiece = false;
-        this.slots[current_slot_id].piece = null;
-        passTurn();
         return true;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    public void CapturedPiece(int slot)
+    {
+        Piece p = Grid[slot - 1];
+        
+        controller.capturepiece(p);
+        
+    }
+    public int[] getadjacency(int slot)
+    {
+        int[] a = new int[3];
+        for(int i = 0; i < 3; i++)
+        {
+            a[i] = adjacency[slot-1, i];
+        }
+        return a;
+    }
+
+    public void rollup()
+    {
+        if (SelectedPiece)
+        {
+            if (SelectedPiece.rollup())
+            {
+                DeselectPiece();
+                SwitchTurn();
+            }
+        }
+    }
+
+    public void rolldown()
+    {
+        if (SelectedPiece)
+        {
+            if(SelectedPiece.rolldown()){
+                DeselectPiece();
+                SwitchTurn();
+            }
+        }
+        
+    }
+
+    public void win()
+    {
+        this.winstats = true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ Sinoda/Assets/Scripts/GameController.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using InnerDriveStudios.DiceCreator;
+using UnityEngine;
+/*
+ * Main game Controller
+ * Start game, End game
+ * 
+ */
+[RequireComponent(typeof(PieceGenerater))]
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private BigBoardLayout layout;
+    [SerializeField] private Board board;
+    [SerializeField] private int Player_number;
+    private List<Player> Players;
+    public int ActivePlayer;
+    private PieceGenerater pieceCreator;
+    
+    private void Awake()
+    {
+        pieceCreator = GetComponent<PieceGenerater>();
+        
+    }
+    private void Start()
+    {
+        CreatePlayer();
+        Startthegame();
+        
+        ActivePlayer = 0;
+        GenerateAllPossibleMoves(Players[ActivePlayer]);
+        this.board.SetController(this);
+    }
+
+    // Update is called once per frame
+    private void Startthegame()
+    {
+        for (int i = 0; i < layout.GetPiecesCount(); i++)
+        {
+            
+            int owner = layout.GetPieceOwnerteamAtIndex(i);
+            int value = layout.GetPieceValueteamAtIndex(i);
+            int slot = layout.GetPieceSlotAtIndex(i);
+            int turn = board.IfTurnAtSlot(slot);
+            Vector3 squareCoords = board.GetPositionAtSlot(slot);
+            Piece newp = pieceCreator.CreatePiece(owner).GetComponent<Piece>();
+            newp.SetData(squareCoords, owner, value, slot, turn,i,board);
+            this.board.AddPieceToBoard(slot, newp);
+            //===========================================================================
+            Players[owner].addPiece(newp);
+        }
+    }
+
+    private void CreatePlayer()
+    {   //If u need add AI, add it here
+        Players = new List<Player>();
+        for (int i=0; i<Player_number;i++)
+        {
+            Players.Add(new Player(i, this.board));
+        }
+    }
+
+    private void GenerateAllPossibleMoves(Player p)
+    {
+        p.AllPossibleMoves();
+    }
+
+    public void SwitchTurn()
+    {
+        
+        ActivePlayer++;
+        if (CheckStats())
+        {
+            win(); 
+        }
+        if (ActivePlayer == Player_number)
+        {
+            ActivePlayer = 0;
+        }
+        GenerateAllPossibleMoves(Players[ActivePlayer]);
+    }
+    public bool IfTurnTrue(int i)
+    {
+        return ActivePlayer == i;
+    }
+
+    public void capturepiece(Piece p)
+    {
+        Players[p.owner].removePiece(p);
+        Players[ActivePlayer].Addpoint(p.value);
+        Destroy(p.gameObject);
+    }
+
+    public bool CheckStats()
+    {
+        int count = 0;
+        int countone = 0;
+        for (int i = 0; i < Player_number; i++)
+        {
+            if (Players[i].zeroleft())
+            {
+                count++;
+            }
+            if (Players[i].oneleft())
+            {
+                countone++;
+            }
+        }
+        if(count == Player_number - 1)//if one player left
+        {
+            return true;
+        }
+        if(count == Player_number -2 && countone == 2)// if last two both one piece left
+        {
+            return true;
+        }
+        return false;
+        
+    }
+
+    private void win()
+    {   //What happen when winning?
+        // TODO
+        board.win();
+        int maxpiont = 0;
+        int player = -1;
+        for (int i = 0; i < Player_number; i++)
+        {
+            if (Players[i].score > maxpiont)
+            {
+                maxpiont = Players[i].score;
+                player = i;
+            }
+        }
+        Debug.Log("player:" + player + "win");
+    }
+}
